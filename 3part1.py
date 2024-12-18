9dcb456088ea6fcbb64ca0df8d5e153b126cffce
Luis Yudi <luisyudi1@gmail.com>
2024-12-17 21:25:30 -0300

    Day 3 part 1 done without loading input on memory

--- 3part1.py
+++ 3part1.py
@@ -1,15 +1,50 @@
-import re
-input = open("Inputs\Day3Input.txt", "r")
-input = input.read()
+total = 0
+instruction_step = 0
+param_one = ""
+param_two = ""
+start_param_two = False
+mult = "mul("
 
-total_sum = 0
+def restart():
+    global instruction_step, param_one, param_two, start_param_two
+    instruction_step = 0
+    param_one = ""
+    param_two = ""
+    start_param_two = False
 
-pattern = "mul\(\d{1,3}\,\d{1,3}\)"
+with open("Inputs\Day3Input.txt") as input:
+    while True:
+        c = input.read(1)
 
-x = re.findall(pattern, input)
+        #EOF
+        if not c:
+            break
+        
+        #Check character
+        if instruction_step < 4 and c == mult[instruction_step]:
+            instruction_step += 1
+            continue
+        
+        if start_param_two:
+            if len(param_two) < 3 and c.isnumeric():
+                param_two += c 
+                continue
+            elif c == ")" and len(param_two) != 0: #If c == ")" and param2 is not null
+                conta = int(param_one) * int(param_two)
+                print(param_one + "*" + param_two)
+                total += int(param_one) * int(param_two)
+        elif instruction_step > 3:
+            #If c is a number for param 1
+            if len(param_one) < 3 and c.isnumeric() and start_param_two == False:
+                param_one += c 
+                continue
+            elif c == "," and len(param_one) != 0: #If c == "," and param1 is not null
+                start_param_two = True
+                continue
+        
+        
+        restart()
+        if c == "m":
+            instruction_step += 1
 
-for command in x:
-    mult = int(command[4:command.index(",")]) * int(command[command.index(",") + 1:command.index(")")])
-    total_sum += mult
-
-print(total_sum)
+print(total)
